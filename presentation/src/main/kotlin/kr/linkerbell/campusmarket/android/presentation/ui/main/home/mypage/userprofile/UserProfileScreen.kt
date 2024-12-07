c8dea068af1e0b59bbb70527403de07859aaded5
Ray Jang <ajou4095@gmail.com>
2024-12-07 21:33:09 +0900

    사용자 정보 스크롤 추가

--- presentation/src/main/kotlin/kr/linkerbell/campusmarket/android/presentation/ui/main/home/mypage/userprofile/UserProfileScreen.kt
+++ presentation/src/main/kotlin/kr/linkerbell/campusmarket/android/presentation/ui/main/home/mypage/userprofile/UserProfileScreen.kt
@@ -18,8 +18,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardDefaults.cardElevation
@@ -98,6 +100,7 @@ fun UserProfileScreen(
         modifier = Modifier
             .background(White)
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         val (topBar, contents) = createRefs()
         Row(
